158faf89dac71cbd8b32db95f1ba5f48d9265273
C.A.Tucker <cartheur@pm.me>
2023-03-23 15:58:57 +0100

    Add IP warning

--- src/Aeon.Recognizer/MainForm.cs
+++ src/Aeon.Recognizer/MainForm.cs
@@ -1,3 +1,6 @@
+//
+// Those parts of the recognizer belonging to the aeon AGI are the intellectual property of Dr. Christopher A. Tucker. Copyright 2023, all rights reserved. No rights are explicitly granted to persons who have obtained this source code.
+//
 namespace Aeon.Recognizer
 {
     public partial class MainForm : Form
